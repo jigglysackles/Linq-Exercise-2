0b5ecc42549d0b349d1799f14b7470a46972d08b
John Hoffman <gnosisadvent@live.com>
2026-06-15 14:39:21 -0500

    Partial completion

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -21,23 +21,53 @@ namespace LinqExercise
              */
 
             //TODO: Print the Sum of numbers
+            var sumNumbers = numbers.Sum();
+            Console.WriteLine("+== Sum of Numbers ==+");
+            Report.Out(sumNumbers);
 
             //TODO: Print the Average of numbers
+            var avgNumbers = numbers.Average();
+            Console.WriteLine("+== Average of Numbers ==+");
+            Report.Out(avgNumbers);
 
             //TODO: Order numbers in ascending order and print to the console
+            var ascNumbers = numbers.OrderBy(x => x).ToList();
+            Console.WriteLine("+== Ascending Numbers ==+");
+            Report.Out(ascNumbers);
 
             //TODO: Order numbers in descending order and print to the console
+            var descNumbers = numbers.OrderByDescending(x => x).ToList();
+            Console.WriteLine("+== Descending Numbers ==+");
+            Report.Out(descNumbers);
 
             //TODO: Print to the console only the numbers greater than 6
+            var overSixNumbers = numbers.Where(x => x > 6).ToList();
+            Console.WriteLine("+== Numbers Greater than Six ==+");
+            Report.Out(overSixNumbers);
 
             //TODO: Order numbers in any order (ascending or desc) but only print 4 of them **foreach loop only!**
+            var fourDescNumbers = descNumbers.GetRange(0, 4);
+            Console.WriteLine("+== Four Descending numbers ==+");
+            Report.Out(fourDescNumbers);
 
             //TODO: Change the value at index 4 to your age, then print the numbers in descending order
+            numbers[4] = 42;
+            descNumbers = numbers.OrderByDescending(x => x).ToList();
+            Console.WriteLine("+== Swapped index[4] value of 5 for Age ==+");
+            Report.Out(descNumbers);
 
             // List of employees ****Do not remove this****
             var employees = CreateEmployees();
 
             //TODO: Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S and order this in ascending order by FirstName.
+            var fullNameOfCAndS = employees.Where(x => x.FirstName.StartsWith('S') || x.FirstName.StartsWith('C'));
+            var compiledEmployeeList = fullNameOfCAndS.OrderBy(x => x.FirstName).ToList();
+            Console.WriteLine("+==  ==+");
+            // Report.Out(compiledEmployeeList);
+            foreach (var item in compiledEmployeeList)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+            }
 
             //TODO: Print all the employees' FullName and Age who are over the age 26 to the console and order this by Age first and then by FirstName in the same result.
 
---
+++ LinqExercise/Report.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqExercise;
+
+public class Report
+{
+    public static void Out(int[] arr)
+    {
+        foreach (var e in arr)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    public static void Out(double[] arr)
+    {
+        foreach (var e in arr)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public static void Out(List<int> arr)
+    {
+        foreach (var e in arr)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    public static void Out(List<string> arr)
+    {
+        foreach (var e in arr)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    // internal static void Out(List<Employee> employeeList)
+    // {
+    //     foreach (var e in employeeList)
+    //     {
+    //         Console.WriteLine(e);
+    //     }
+    // }
+
+    public static void Out(int n)
+    {
+        Console.WriteLine(n);
+    }
+
+    public static void Out(double n)
+    {
+        Console.WriteLine(n);
+    }
+    
+    public static void Out(decimal n)
+    {
+        Console.WriteLine(n);
+    }
+    
+}
